7afc4e6a2fd8f5a794ccd414da0d55a1501aec35
rrichajalota <rrichajalota@gmail.com>
2016-09-18 02:52:36 +0530

    added socialmedia.py

---
+++ socialmedia.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python3
+import sys, webbrowser, pyperclip
+twitter = 'www.twitter.com'
+facebook = 'www.facebook.com'
+webbrowser.open('https://'+twitter)
+webbrowser.open('https://'+facebook)
